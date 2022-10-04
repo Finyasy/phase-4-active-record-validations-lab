dd756e6dbc4d169cc31b78b83b780a0acccc8d69
Bryan Bosire <bryanbosire@gmail.com>
2022-10-04 08:24:52 +0300

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+     # Basic validations
+    # is expected to validate that :name cannot be empty/falsy (FAILED - 1)
+    validates :name, presence: true
+    # is expected to validate that :name is case-sensitively unique (FAILED - 2)
+    validates :name, uniqueness: true
+    # is expected to validate that the length of :phone_number is 10 (FAILED - 3)
+    validates :phone_number, length: { is: 10 }
 end
